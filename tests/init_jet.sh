2ee6de4afdc2bc3aa56c1b88370172a0908c0095
Gerhard Preuss <gelipp@gmail.com>
2015-02-13 12:24:30 +0100

    reuse start script

--- tests/init_jet.sh
+++ tests/init_jet.sh
@@ -4,7 +4,10 @@ if [ -f /tmp/init_jet ]; then
 else
     echo "Starting Jet!"
     node node_modules/.bin/jetd.js &
+    sleep 1
     node node_modules/.bin/some-service.js &
+    sleep 1
     node node_modules/node-jet/examples/todo-server.js 1234 &
+    sleep 1
     touch /tmp/init_jet
 fi
